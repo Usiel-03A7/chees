871cd3fed6bd1db1f272c8ab428eb7163947f234
Usiel-03A7 <shebastyanmail.com>
2023-01-29 21:45:07 -0600

    agregamos la validacion de colores para el movimiento de captura

--- js/app.js
+++ js/app.js
@@ -18,17 +18,17 @@ const cuadros = document.querySelector('.cuadros');
 
 Object.entries(tablero.casillas).filter((element) => {
     if (element[1].pieza != null) {
-        
+
         return true
     }
     return false
-    
+
 }).forEach((element) => { //en el objeto tablero accedimos a su objeto casillas
     //
     const coordenada = element[0];
     const pieza = element[1].pieza;
-    
-    
+
+
     cuadros.querySelector('#' + coordenada).textContent = pieza.getFigura();
 })
 // console.log(tablero.casillas);
@@ -38,118 +38,155 @@ const invertir = document.getElementById('invertir');
 
 invertir.addEventListener('click', () => {
     const arregloCuadros = cuadros.querySelectorAll('div');
-    
+
     // for (let i = arregloCuadros.length -1; i >= 0; i--) {
-        
-        //     console.log(arregloCuadros[i]);
-        
-        // }
-        const peon = new Peon('blanco')
-        const barRight = document.querySelector('.barRight');
-        const barDown = document.querySelector('.barDown');
-        const barUp = document.querySelector('.barUp');
-        const barLeft = document.querySelector('.barLeft');
-        
-        barRight.classList.toggle('reversa');
-        barDown.classList.toggle('reversa');
-        barUp.classList.toggle('reversa');
-        barLeft.classList.toggle('reversa');
-        
-        for (let i = 0; i < arregloCuadros.length; i++) {
-            let aux = arregloCuadros[i]
-            let aux2 = arregloCuadros[arregloCuadros.length - i - 1]
-            cuadros.removeChild(aux)
-            cuadros.removeChild(aux2)
-            cuadros.insertAdjacentElement("afterbegin", aux)
-            cuadros.insertAdjacentElement("beforeend", aux2)
-        }
-        
-    })
+
+    //     console.log(arregloCuadros[i]);
+
+    // }
+    const peon = new Peon('blanco')
+    const barRight = document.querySelector('.barRight');
+    const barDown = document.querySelector('.barDown');
+    const barUp = document.querySelector('.barUp');
+    const barLeft = document.querySelector('.barLeft');
+
+    barRight.classList.toggle('reversa');
+    barDown.classList.toggle('reversa');
+    barUp.classList.toggle('reversa');
+    barLeft.classList.toggle('reversa');
+
+    for (let i = 0; i < arregloCuadros.length; i++) {
+        let aux = arregloCuadros[i]
+        let aux2 = arregloCuadros[arregloCuadros.length - i - 1]
+        cuadros.removeChild(aux)
+        cuadros.removeChild(aux2)
+        cuadros.insertAdjacentElement("afterbegin", aux)
+        cuadros.insertAdjacentElement("beforeend", aux2)
+    }
+
+})
+
+
+
+
+
+cuadros.addEventListener('click', (e) => {
+    let cuadroElectoAnterior = cuadros.querySelector('.cuadroElecto')
+
+    limpiar();
+
+    mover(e, cuadroElectoAnterior);
+
     
-    
-  
-   
-   cuadros.addEventListener('click', (e) => {
-       let cuadroElectoAnterior = cuadros.querySelector('.cuadroElecto')
-       
-       limpiar();
-       
-       mover(e,cuadroElectoAnterior);
-       
-       
-    })
-    
-    function limpiar() {
-        let cuadroElectoAnterior = cuadros.querySelector('.cuadroElecto')
-        let movimientoElectoAnterior = cuadros.querySelectorAll('.movimiento')
-    
-    if (cuadroElectoAnterior) {        
+
+
+
+})
+
+function limpiar() {
+    let cuadroElectoAnterior = cuadros.querySelector('.cuadroElecto')
+    let movimientoElectoAnterior = cuadros.querySelectorAll('.movimiento')
+    let movimientoCapturar = cuadros.querySelectorAll('.capturar')
+
+    if (cuadroElectoAnterior) {
         cuadroElectoAnterior.classList.remove('cuadroElecto')
-        movimientoElectoAnterior.forEach((nodo)=>{
+        movimientoElectoAnterior.forEach((nodo) => {
             nodo.classList.remove('movimiento')
+        })
+        movimientoCapturar.forEach((nodo) => {
+            nodo.classList.remove('capturar')
         })
     }
 }
-function mover(e,cuadroElectoAnterior) {
-    
-    let cuadroElecto = e.target
+    function mover(e, cuadroElectoAnterior) {
 
-    
-    if(cuadroElectoAnterior ){
+        let cuadroElecto = e.target
 
-        if (cuadroElectoAnterior.id===cuadroElecto.id) {
-            limpiar();
-            return
-        }
-        else{
-            let movimientoPermitido = tablero.casillas[cuadroElectoAnterior.id].pieza.move(cuadroElectoAnterior.id)
-            
-            if(!movimientoPermitido.some(id=>{
-                return id===cuadroElecto.id
-            })){
-                return 
+
+        if (cuadroElectoAnterior) {
+
+            if (cuadroElectoAnterior.id === cuadroElecto.id) {
+                limpiar();
+                return
             }
-            let AuxPieza =  tablero.casillas[cuadroElectoAnterior.id].pieza
-            AuxPieza.contador ++;
-            tablero.casillas[cuadroElecto.id].pieza = AuxPieza;
-            tablero.casillas[cuadroElectoAnterior.id].pieza = null;
-            cuadroElecto.textContent=cuadroElectoAnterior.textContent;
-            cuadroElectoAnterior.textContent = '';
-            return
+            else {
+                let movimientoPermitido = tablero.casillas[cuadroElectoAnterior.id].pieza.move(cuadroElectoAnterior.id)
 
+                if (!movimientoPermitido.some(id => {
+                    return id === cuadroElecto.id
+                })) {
+                    return
+                }
+                if (movimientoPermitido.some(id => {
+                    
+                })) {
+
+                }
+                let AuxPieza = tablero.casillas[cuadroElectoAnterior.id].pieza
+                AuxPieza.contador++;
+                tablero.casillas[cuadroElecto.id].pieza = AuxPieza;
+                tablero.casillas[cuadroElectoAnterior.id].pieza = null;
+                cuadroElecto.textContent = cuadroElectoAnterior.textContent;
+                cuadroElectoAnterior.textContent = '';
+                return
+
+            }
         }
-    }
 
 
-    if (!tablero.casillas[cuadroElecto.id].pieza) {
-        return
-    }
+        if (!tablero.casillas[cuadroElecto.id].pieza) {
+            return
+        }
 
-    cuadroElecto.classList.add('cuadroElecto');
+        cuadroElecto.classList.add('cuadroElecto');
 
-    
-    let posibleMovimiento = tablero.casillas[cuadroElecto.id].pieza.move(cuadroElecto.id)
-    //console.log(posibleMovimiento)
-    posibleMovimiento.forEach(id=>{ 
-        cuadros.querySelector('#'+id).classList.add('movimiento')
-    })
-    
 
-    //tablero.casillas[cuadroElectoAnterior]
-    
-}
-
-// function PiezaConReglas(pieza) {
-    
-//     let figuras = [ '♞', '♘','♟', '♙', '♜', '♖', '♚', '♔', '♛', '♕', '♝', '♗'];
-//     let piezaBuscada = pieza
-//     for (let i = 0; i < figuras.length; i++) {
+        let posibleMovimiento = tablero.casillas[cuadroElecto.id].pieza.move(cuadroElecto.id)
         
-//         if (piezaBuscada == figuras[i]) {
-//             console.log('si está en el arreglo');      
-//         }
-//     }
-// }
+        if(posibleMovimiento.some(element=>{
+            return tablero.casillas[element]
+        })){}
+        //console.log(posibleMovimiento)
+        /*
+
+        posibleMovimiento.forEach(elemento, id => {
+            if(posibleMovimiento)
+        })
+        */
+
+        posibleMovimiento.forEach((coordenada,id) => {
+            if (!tablero.casillas[coordenada].pieza) {
+                cuadros.querySelector('#' + coordenada).classList.add('movimiento')   
+            }
+            else {
+                if (tablero.casillas[coordenada].pieza.color != tablero.casillas[cuadroElecto.id].pieza.color) {
+                    cuadros.querySelector('#' + coordenada).classList.add('capturar'); 
+                }
+            }
+            
+            
+            // let piezaVacia = 
+            // if (coordenada) {
+                    
+            //     }
+        })
+
+
+        //tablero.casillas[cuadroElectoAnterior]
+
+    }
+
+    // function PiezaConReglas(pieza) {
+
+    //     let figuras = [ '♞', '♘','♟', '♙', '♜', '♖', '♚', '♔', '♛', '♕', '♝', '♗'];
+    //     let piezaBuscada = pieza
+    //     for (let i = 0; i < figuras.length; i++) {
+
+    //         if (piezaBuscada == figuras[i]) {
+    //             console.log('si está en el arreglo');      
+    //         }
+    //     }
+    // }
 
 
 
