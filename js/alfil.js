c087459b1931439577be943031d13a48b8f87bda
Usiel-03A7 <shebastyanmail.com>
2023-02-07 22:24:40 -0600

    para clonar

--- js/alfil.js
+++ js/alfil.js
@@ -4,12 +4,34 @@ class Alfil extends Pieza {
      * @param {'blanco' | 'negro'} color 
      */
     constructor(color) {
-        const reglas = new Reglas([
-            new Coordenada(-1,+1),                          new Coordenada(+1,+1),
-                                                          
-            new Coordenada(-1,-1),                         new Coordenada(+1,-1)
-        ], true);
-        super(color,'Alfil',reglas)
+        let arregloMovimientos = [new Coordenada(-1, +1), new Coordenada(+1, +1), new Coordenada(-1, -1), new Coordenada(+1, -1)]
+        const arregloDeArregloCordenada = arregloMovimientos.map(cordenada => {
+            let ArregloAux = [];
+            for (let i = -1; i <= 7; i++) {
+
+                let cordenadaAux = new Coordenada(cordenada.x, cordenada.y)
+
+                if (cordenada.x < 0) {
+                    cordenadaAux.x = cordenadaAux.x - i - 1
+                } else {
+                    cordenadaAux.x = cordenadaAux.x + i + 1
+                }
+
+                if (cordenada.y < 0) {
+                    cordenadaAux.y = cordenadaAux.y - i - 1
+                } else {
+                    cordenadaAux.y = cordenadaAux.y + i + 1
+
+                }
+
+                ArregloAux.push(cordenadaAux)
+            }
+            return ArregloAux
+
+        })
         
-    } 
+        const reglas = new Reglas(arregloDeArregloCordenada.flat(),true);
+        super(color, 'Alfil', reglas)
+
+    }
 }
--- js/torre.js
+++ js/torre.js
@@ -4,11 +4,33 @@ class Torre extends Pieza {
      * @param {'blanco' | 'negro'} color 
      */
     constructor(color) {
-        const reglas = new Reglas([
-                                 new Coordenada(0,1),
-            new Coordenada(-1,0),                    new Coordenada(1,0),
-                                new Coordenada(0,-1),
-        ], true);
+        let arregloMovimientos= [new Coordenada(0,1),new Coordenada(-1,0),new Coordenada(1,0),new Coordenada(0,-1)]
+
+        const arrayDeArrayMovimientos = arregloMovimientos.map(cordenada=>{
+            let arrayAux =[];
+            for (let i = 0; i <=7; i++) {
+                if (cordenada.x!=0) {
+                    if (cordenada.x < 0) {
+                        arrayAux.push(new Coordenada(i+1,cordenada.y))    
+                    }
+                    else{
+                        
+                        arrayAux.push(new Coordenada(-i-1,cordenada.y)) 
+                    }
+                } 
+                if(cordenada.y!=0){
+                    if (cordenada.y < 0) {
+                        arrayAux.push(new Coordenada(cordenada.x,-i-1))    
+                    }
+                  else{
+                    arrayAux.push(new Coordenada(cordenada.x,i+1))
+                  }
+                }        
+            }
+            return arrayAux
+        })
+
+        const reglas = new Reglas(arrayDeArrayMovimientos.flat(), true);
         super(color,'Torre',reglas)
         
     }  
